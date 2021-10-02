f9aef96be8a29decd8fa4600aa37283cf7c123ea
user1 <user1@user.fios-router.home>
2021-10-02 11:07:22 -0400

    made changes to sat and sun

--- src/saturday/DemoSaturday.java
+++ src/saturday/DemoSaturday.java
@@ -1,6 +1,10 @@
 package saturday;
 
 public class DemoSaturday {
+	
+	public static void addnum( int a, int b) {
+		System.out.println("a+b");
+	}
 	 public static void main (String[] args) {
 		 System.out.println("Sunday Demo....");
 //Changes made mt a team member
